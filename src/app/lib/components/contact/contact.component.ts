c264d46d0c4253722914ac3b29f1b69d9f013434
Erick Gaspar <egaspar@sccalpha.local>
2019-09-27 09:18:28 -0300

    crud contacts

---
+++ src/app/lib/components/contact/contact.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  @Input() menuValue: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/lib/components/contact/contact.service.ts
+++ src/app/lib/components/contact/contact.service.ts
@@ -1,28 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from "rxjs/operators";
+import { map } from 'rxjs/operators';
+import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class ContactService {
-
-    private URLbase = 'https://localhost:3000/';
-
     private API_KEY = 'e56d3ca5d6ab8eb246a64d966fb72b5cde48b740';
 
-    constructor(private httpClient: HttpClient) { }
+    private URLbase = 'http://localhost:3000/';
+    public contactList;
+
+    private contactBehavior = new BehaviorSubject<any>(new Object());
+    contact = this.contactBehavior.asObservable();
+
+
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    async updateContact(contact: any) {
+        console.log('contact service - contact: ' + contact);
+        this.contactBehavior.next(contact);
+    }
 
     public list() {
-        return this.httpClient.get(this.URLbase + `contact/?key=${this.API_KEY}`);
+        this.contactList = this.httpClient.get(this.URLbase + `contact/?key=${this.API_KEY}`);
+        return this.contactList;
     }
-    public add(contact) {
+    public async add(contact) {
         return this.httpClient.post(this.URLbase + 'contact', contact, {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json',
             })
         }).pipe(map(data =>
             data));
-
+    }
+    public delete(contactId) {
+        return this.httpClient.delete(this.URLbase + `contact/${contactId}`);
+    }
+    public async update(contact) {
+        return this.httpClient.put(this.URLbase + 'contact', contact, {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+            })
+        }).pipe(map(data =>
+            data));
     }
 }
