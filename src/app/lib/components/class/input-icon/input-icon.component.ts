ffc2e551a452408d970056dd869bc3a41f88fb1b
Erick Souza Gaspar <erickgaspar@sisconsultoria.com.br>
2019-10-19 00:01:58 -0300

    componente input-icon

---
+++ src/app/lib/components/class/input-icon/input-icon.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'congr-input-icon',
+  templateUrl: './input-icon.component.html',
+  styleUrls: ['./input-icon.component.scss']
+})
+export class InputIconComponent implements OnInit {  
+  
+  constructor() {}
+  
+  @Input() inputModel: string;  
+  @Input() type: string;
+  @Input() icon: string;
+  @Input() id: string;
+  
+  @Output() inputModelChange = new EventEmitter<string>();  
+  
+    
+  ngOnInit() {  
+  }  
+  
+  change(){  
+    this.inputModelChange.emit(this.inputModel);  
+  }  
+  
+}  
