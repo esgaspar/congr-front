45b18e0adef94364993495af86c9a171babcbcd8
Erick S. Gaspar <erickgaspar@sisconsultoria.com.br>
2019-11-21 11:04:35 -0300

    heroku

--- src/app/lib/components/contact/contact.service.ts
+++ src/app/lib/components/contact/contact.service.ts
@@ -50,4 +50,19 @@ export class ContactService {
         }).pipe(map(data =>
             data));
     }
+
+
+    getPosition(): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            navigator.geolocation.getCurrentPosition(resp => {
+
+                resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+            },
+                err => {
+                    reject(err);
+                });
+        });
+
+    }
 }
