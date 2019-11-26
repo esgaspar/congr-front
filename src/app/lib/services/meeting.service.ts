b36d529eb5c036f43233270e4c23ed1c77d19456
Erick S. Gaspar <erickgaspar@sisconsultoria.com.br>
2019-11-26 14:20:36 -0300

    login

--- src/app/lib/services/meeting.service.ts
+++ src/app/lib/services/meeting.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { environment } from './../../../../environments/environment'
+import { environment } from '../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
